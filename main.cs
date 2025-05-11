d08040d902f084c21ecbe2e0f9b6be9695f63643
tairasoul <100938045+tairasoul@users.noreply.github.com>
2025-05-11 06:45:07 +0200

    forgot to commit this

--- main.cs
+++ main.cs
@@ -5,7 +5,14 @@ using Newtonsoft.Json;
 Utils.initializeExterns();
 
 string configPath = Path.Join(Directory.GetCurrentDirectory(), "pdsl.json");
-string configData = File.ReadAllText(configPath);
-Config cfg = JsonConvert.DeserializeObject<Config>(configData) ?? new Config("./src", "./dist");
+Config cfg;
+if (File.Exists(configPath)) {
+  string configData = File.ReadAllText(configPath);
+  cfg = JsonConvert.DeserializeObject<Config>(configData) ?? new Config("./src", "./dist");
+}
+else 
+{
+  cfg = new Config("./src", "./dist");
+}
 PdslCompiler compiler = new(cfg);
 compiler.compile();
